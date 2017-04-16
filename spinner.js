9ac41e5ed391fbb7ee7cf3898c5f6b1cd5b0f389
Seth Van Roekel <seth@exomut.com>
2017-04-16 21:01:41 +0900

    Fixed my late night mistakes.

--- spinner.js
+++ spinner.js
@@ -1,21 +1,33 @@
 var canvas;
 var context;
 var timer;
-var pin = new Image();
+
+var spinner;
+
+function Spinner () {
+  this.speed = 0.0;
+  this.image = new Image();
+  this.image.src = "./frabby.png";
+
+  this.draw = function(context){
+    context.drawImage(this.image, -this.image.width/2, -this.image.height/2);
+  };
+};
 
 
 function init() {
-  pin.src = "./frabby.png";
   canvas = document.getElementById("canvas");
   context = canvas.getContext("2d");
-  timer = setInterval(draw, 10);
+  timer = setInterval(draw, 30);
+  spinner = new Spinner();
   return timer;
 }
 
 function draw() {
-  context.clearRect(0, 0, canvas.width, canvas.height);
+  context.fillStyle = "#99CCFF";
+  context.fillRect(0, 0, canvas.width, canvas.height);
   context.translate(canvas.width/2, canvas.height/2);
-  context.rotate(.1);
-  context.drawImage(pin, -pin.width/2, -pin.height/2);
+  context.rotate(.6);
+  spinner.draw(context);
   context.translate(-canvas.width/2, -canvas.height/2);
 }
