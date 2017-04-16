825e5d150b2be9a265d5931ba225b72038b5a7be
Seth Van Roekel <seth@exomut.com>
2017-04-16 22:30:23 +0900

    Touch and mouse support added. Cleaned up code.

--- spinner.js
+++ spinner.js
@@ -1,33 +1,44 @@
 var canvas;
 var context;
+var spinner;
 var timer;
 
-var spinner;
-
 function Spinner () {
-  this.speed = 0.0;
   this.image = new Image();
-  this.image.src = "./frabby.png";
+  this.image.src = "./spinner.png";
+  this.speed = 0.5;
+  this.isSpinning = false;
 
   this.draw = function(context){
+    // Translate the canvas so it rotates around the center
+    context.translate(canvas.width/2, canvas.height/2);
+
+    if (this.isSpinning) context.rotate(this.speed);
+
     context.drawImage(this.image, -this.image.width/2, -this.image.height/2);
+    context.translate(-canvas.width/2, -canvas.height/2);
   };
 };
 
+function spin() {
+  spinner.isSpinning = spinner.isSpinning ? false : true;
+}
 
 function init() {
   canvas = document.getElementById("canvas");
   context = canvas.getContext("2d");
-  timer = setInterval(draw, 30);
+
+  // Setup touch and mouse support to call the spin function
+  document.body.addEventListener("mouseup", spin, false);
+  document.body.addEventListener("touchup", spin, false);
+
   spinner = new Spinner();
-  return timer;
+
+  // Call the draw function 30 times a second
+  setInterval(draw, 1000/30);
 }
 
 function draw() {
-  context.fillStyle = "#99CCFF";
-  context.fillRect(0, 0, canvas.width, canvas.height);
-  context.translate(canvas.width/2, canvas.height/2);
-  context.rotate(.6);
+  context.clearRect(0, 0, canvas.width, canvas.height);
   spinner.draw(context);
-  context.translate(-canvas.width/2, -canvas.height/2);
 }
